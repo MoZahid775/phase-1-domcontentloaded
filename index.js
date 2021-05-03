3694d20aadf2e3de147511cdfb80b75fd132ecf1
Muhammad Zahid <muhammadali775@gmail.com>
2021-05-02 20:01:49 -0400

    commit tree 3686fb6472a06d7911b64cba326138814b3ec8db

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+
+const element = document.getElementById('text')
+
+
+
+
+function change(){ 
+document.addEventListener("DOMContentLoaded", function() {
+    
+ element.remove()
+ 
+ let newP = document.createElement('p')
+ newP.id = "text"
+ newP.textContent = "This is really cool!"
+ document.body.appendChild(newP)
+
+
+  }); 
+
+}
+
+change();
+
+
+
+
